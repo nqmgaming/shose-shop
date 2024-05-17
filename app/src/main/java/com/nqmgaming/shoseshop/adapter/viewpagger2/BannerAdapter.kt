ce05b60663c212f46bad7faab4a2a4b647df9f98
Nguyen Quang Minh <nguyenquangminh570@gmail.com>
2024-05-17 18:58:30 +0700

    logo, search view, banner

---
+++ app/src/main/java/com/nqmgaming/shoseshop/adapter/viewpagger2/BannerAdapter.kt
@@ -0,0 +1,30 @@
+package com.nqmgaming.shoseshop.adapter.viewpagger2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.nqmgaming.shoseshop.R
+
+class BannerAdapter(private val images: List<Int>) : RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
+
+    inner class BannerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_banner, parent, false)
+        return BannerViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
+        holder.imageView.setImageResource(images[position])
+    }
+}
--- app/src/main/java/com/nqmgaming/shoseshop/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/nqmgaming/shoseshop/ui/fragments/HomeFragment.kt
@@ -6,18 +6,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nqmgaming.shoseshop.R
+import com.nqmgaming.shoseshop.adapter.viewpagger2.BannerAdapter
+import com.nqmgaming.shoseshop.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-
-
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.bannerViewpager.adapter = BannerAdapter(
+            listOf(
+                R.drawable.banner1,
+                R.drawable.banner2,
+                R.drawable.banner3
+            )
+        )
+        binding.indicator.setViewPager2(binding.bannerViewpager)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
